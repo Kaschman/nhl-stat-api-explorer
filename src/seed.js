124fecad3c3b4a41c58a539a269626775a25dd44
Steve Kaschinske <steve.kaschinske@gmail.com>
2019-06-10 13:46:28 -0600

    Seeds teams from NHL API

--- src/seed.js
+++ src/seed.js
@@ -1,12 +1,29 @@
-import superagent from "superagent"
+import superagent from 'superagent'
+import { database } from './config'
+
+import type { Team } from './models/team'
+import { serialize, toSQL } from './models/team'
 
 superagent.get('https://statsapi.web.nhl.com/api/v1/teams')
-.end((error, response) => {
+.end(async (error, response) => {
   if (error) {
     console.log('Error Occured')
     console.log(error)
-  }
-  else {
-    console.log(response.body)
+  } else {
+    await database('teams').del()
+    const teams: Team[] = response.body.teams.map(team => serialize(team))
+
+    const temp = teams.map(team => toSQL(team))
+    await database('teams').insert(temp)
+
+    process.exit()
   }
 })
+
+const deleteTeams = async () => {
+  return
+}
+
+const saveTeams = async (teams: Team[]) => {
+
+}
